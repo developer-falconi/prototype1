42dd2a2d217f1e713e1d538eda082a97983258de
genafalconi <genaro.f@hotmail.com>
2023-11-29 23:05:11 -0300

    fixes

--- src/helpers/cloudinary.js
+++ src/helpers/cloudinary.js
@@ -2,9 +2,9 @@ import { Cloudinary } from "@cloudinary/url-gen";
 
 const cloudinary = new Cloudinary({
   cloud: {
-    cloudName: process.env.REACT_APP_CLOUD_NAME,
-    apiKey: process.env.REACT_APP_API_KEY,
-    apiSecret: process.env.REACT_APP_API_SECRET,
+    cloudName: process.env.REACT_APP_CLOUDINARY_CLOUD_NAME,
+    apiKey: process.env.REACT_APP_CLOUDINARY_API_KEY,
+    apiSecret: process.env.REACT_APP_CLOUDINARY_API_SECRET,
   },
 });
 
