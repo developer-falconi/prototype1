118890bb5f2577f5e429ba4dbb4f11a074535bdd
genafalconi <genaro.f@hotmail.com>
2023-12-01 19:46:56 -0300

    add loader ghosts

--- src/ticket/TicketMain.jsx
+++ src/ticket/TicketMain.jsx
@@ -8,6 +8,7 @@ import { cloudinaryImg } from "../helpers/cloudinary";
 
 const img = 'Vanellus/Ppal/Flyercrop_skfynv'
 const img2 = 'Vanellus/Ppal/Flyer_fgwdm5'
+const img3 = 'Vanellus/Ppal/f03e7805-0443-4b90-be25-414c3f5ef007_variated_wkxufe'
 
 export default function TicketMain() {
 
@@ -16,20 +17,23 @@ export default function TicketMain() {
 
   const getPrevents = useCallback(async () => {
     await GET_PREVENTS().then((res) => {
-      setIsLoading(false)
+      // setIsLoading(false)
       setPrevents(res)
     })
   }, [])
 
   useEffect(() => {
-    getPrevents()
+    getPrevents();
   }, [getPrevents])
 
   return (
     <div className="content-page">
       {
         isLoading ? (
-          <Loader />
+          <>
+            <Loader />
+            <AdvancedImage cldImg={cloudinaryImg(img3)} alt='ghosts' className='ghost-img' />
+          </>
         ) : (
           <>
             <div className="prevent-tickets">
@@ -40,12 +44,11 @@ export default function TicketMain() {
                 })
               }
               <h2 className="info-help">
-                Por cualquier problema comunicarse con
-                <a
+                Por cualquier problema comunicarse con <a
                   href="https://wa.me/+5491161569011?text=Hola!%20Necesito%20entradas%20para%20la%20Fantom"
                   rel="noreferrer"
                   target="_blank"
-                > Mateo</a>
+                >Mateo</a>
               </h2>
             </div>
             <AdvancedImage cldImg={cloudinaryImg(window.innerWidth > 521 ? img : img2)} alt='img' className='flyer-img' />
