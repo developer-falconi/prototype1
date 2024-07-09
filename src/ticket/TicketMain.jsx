c29cd3a01a54d7e5f680f3be358cf8501d55facb
genafalconi <genaro.f@hotmail.com>
2024-07-09 15:27:56 -0300

    new vanellus

--- src/ticket/TicketMain.jsx
+++ src/ticket/TicketMain.jsx
@@ -6,8 +6,8 @@ import { AdvancedImage } from "@cloudinary/react";
 import { cloudinaryImg } from "../helpers/cloudinary";
 import TicketTemplate from "./TicketTemplate";
 
-const img = 'Vanellus/Ppal/Flyercrop_okjuuy.jpg'
-const img2 = 'Vanellus/Ppal/Flyer_fgwdm5'
+const img = 'Flyercrop.jpg'
+const img2 = 'Flyer.jpg'
 // const img3 = 'Vanellus/Ppal/f03e7805-0443-4b90-be25-414c3f5ef007_variated_wkxufe'
 
 export default function TicketMain() {
@@ -39,10 +39,10 @@ export default function TicketMain() {
               <TicketTemplate prevent={prevent} />
               <h2 className="info-help">
                 Por cualquier problema comunicarse con <a
-                  href="https://wa.me/+5491161569011?text=Hola!%20Necesito%20entradas%20para%20la%20Vanellus"
+                  href="https://wa.me/+5491138312454?text=Hola!%20Necesito%20entradas%20para%20la%20Vanellus"
                   rel="noreferrer"
                   target="_blank"
-                >Mateo</a>
+                >Genaro</a>
               </h2>
             </div>
             <AdvancedImage cldImg={cloudinaryImg(window.innerWidth > 521 ? img : img2)} alt='img' className='flyer-img' />
